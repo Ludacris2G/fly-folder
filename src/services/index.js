5ac67624e8eebf85aa60dfb658bdbbe5d09f6a18
Liviu <liviutoma946@gmail.com>
2023-11-16 22:56:15 +0200

    ticket fetching works

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,24 @@
-const { graphql, buildSchema } = require('graphql');
+import request, { gql } from 'graphql-request';
 
-const hygraphToken = process.env.NEXT_PUBLIC_HYGRAPH_TOKEN;
+const hygraphToken = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT;
 
-export const testing = () => {
-  console.log('runs');
+export const testing = async () => {
+  const query = gql`
+    query getTickets {
+      tickets {
+        createdAt
+        from
+        to
+      }
+    }
+  `;
+
+  try {
+    const response = await request(hygraphToken, query);
+    console.log('GQL response: ', response);
+    return response;
+  } catch (error) {
+    console.log('hi');
+    console.error(error);
+  }
 };
