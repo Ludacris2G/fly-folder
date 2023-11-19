b7d4a968c34702d582f32504fdc957213152828d
Liviu <liviutoma946@gmail.com>
2023-11-19 23:12:51 +0200

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='w-full'>
+      <h1 className='text-center'>Sky Folder by Lee</h1>
+    </div>
+  );
+};
+
+export default Navbar;
