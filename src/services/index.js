8fa826c9ad304ef3a705c9cccb977738a7cd369a
Liviu <liviutoma946@gmail.com>
2024-01-07 20:55:02 +0200

    pexels picture links function

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,9 @@
 import request, { gql } from 'graphql-request';
+import { createClient } from 'pexels';
 
 const hygraphToken = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT;
+const pexelsKey = process.env.NEXT_PUBLIC_PEXELS_KEY;
+const client = createClient(pexelsKey);
 
 export const getTickets = async () => {
   const query = gql`
@@ -26,5 +29,15 @@ export const getTickets = async () => {
     return response;
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const getPictureLink = async (place) => {
+  try {
+    const query = place;
+    const response = await client.photos.search({ query, per_page: 1 });
+    if (response) return response.photos[0].src.landscape;
+  } catch (err) {
+    console.log(err);
   }
 };
