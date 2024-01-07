14d75eaf92df880e59d6a092915d274e95659fab
Liviu <liviutoma946@gmail.com>
2024-01-07 20:54:02 +0200

    destination link functionality to display imgs of all destinations

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import TicketCard from '../components/TicketCard';
-import { getTickets } from '../services';
+import { getPictureLink, getTickets } from '../services';
 
 export default function Home() {
   const [tickets, setTickets] = useState([]);
@@ -13,12 +13,19 @@ export default function Home() {
     const tickets = await getTickets();
 
     if (tickets) {
+      const ticketPromises = tickets.tickets.map(async (ticket) => {
+        const link = await getPictureLink(ticket.to);
+
+        ticket.destinationImg = link;
+      });
+
+      await Promise.all(ticketPromises);
       setTickets(tickets.tickets);
     }
   };
 
   return (
-    <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 min-h-screen flex-col items-center justify-between xl:p-24 p-2'>
+    <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 min-h-screen flex-col items-center justify-between xl:p-24 xl:pt-2 p-2'>
       {tickets.length ? (
         <>
           <p className='text-center lg:col-span-3 md:col-span-3 grid-span-1'>
