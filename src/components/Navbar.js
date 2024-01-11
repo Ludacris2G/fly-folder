3de3676fea5b844126ee69f208ed5473c98f2401
Liviu <liviutoma946@gmail.com>
2024-01-11 21:57:42 +0200

    portfolio link, linear gradient

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='w-full'>
-      <h1 className='text-center'>Sky Folder by Lee</h1>
+    <div className='w-full bg-gradient-to-b from-black to-background'>
+      <h1 className='text-center'>
+        Sky Folder by
+        <a
+          className='text-green-400 hover:text-blue-600'
+          target='_blank'
+          href='https://ludacris2g.github.io/'
+        >
+          {' '}
+          Lee
+        </a>
+      </h1>
     </div>
   );
 };
